e9d4b29f80909b20be9f409edc2095eccbfcfb6b
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-11-14 14:57:29 +0530

    bug fix in original codebase: pageIndex datatype changed from string to int

--- src/page.h
+++ src/page.h
@@ -1,25 +1,25 @@
-#include"logger.h"
+#include "logger.h"
 /**
  * @brief The Page object is the main memory representation of a physical page
  * (equivalent to a block). The page class and the page.h header file are at the
- * bottom of the dependency tree when compiling files. 
+ * bottom of the dependency tree when compiling files.
  *<p>
  * Do NOT modify the Page class. If you find that modifications
  * are necessary, you may do so by posting the change you want to make on Moodle
- * or Teams with justification and gaining approval from the TAs. 
+ * or Teams with justification and gaining approval from the TAs.
  *</p>
  */
 
-class Page{
+class Page
+{
 
     string tableName;
-    string pageIndex;
+    int pageIndex;
     int columnCount;
     int rowCount;
     vector<vector<int>> rows;
 
-    public:
-
+public:
     string pageName = "";
     Page();
     Page(string tableName, int pageIndex);
