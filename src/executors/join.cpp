00237526039fe5df5c036c264de80e7b07a799f0
destinyson7 <tanish.lad@research.iiit.ac.in>
2021-11-20 19:26:43 +0530

    removed unneeded functions

--- src/executors/join.cpp
+++ src/executors/join.cpp
@@ -207,23 +207,6 @@ int hashFunction(int val, int M)
     return val % M;
 }
 
-template <class Ch, class Tr, class Container>
-basic_ostream<Ch, Tr> &operator<<(basic_ostream<Ch, Tr> &os, Container const &x)
-{
-    os << "{ ";
-    for (auto &y : x)
-    {
-        os << y << " ";
-    }
-    return os << "}";
-}
-
-template <class X, class Y>
-ostream &operator<<(ostream &os, pair<X, Y> const &p)
-{
-    return os << "[" << p.ff << ", " << p.ss << "]";
-}
-
 vector<int> partition(Table *table, string columnName, int M)
 {
     logger.log("partition");
