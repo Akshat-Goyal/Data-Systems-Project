f780a812b6090a331bdc5ad6fb8d1afbeacf5229
akshat-goyal <akshatgoyalak23@gmail.com>
2021-11-27 17:43:54 +0530

    added sort syntactic and semantic parser

--- src/executors/sort.cpp
+++ src/executors/sort.cpp
@@ -1,56 +1,81 @@
-#include"global.h"
+#include "global.h"
 /**
- * @brief File contains method to process SORT commands.
- * 
- * syntax:
- * R <- SORT relation_name BY column_name IN sorting_order
- * 
- * sorting_order = ASC | DESC 
+ * @brief 
+ * SYNTAX: R <- SORT <table_name> BY <column_name> IN ASC | DESC [BUFFER <buffer_size>]
  */
-bool syntacticParseSORT(){
+bool syntacticParseSORT()
+{
     logger.log("syntacticParseSORT");
-    if(tokenizedQuery.size()!= 8 || tokenizedQuery[4] != "BY" || tokenizedQuery[6] != "IN"){
-        cout<<"SYNTAX ERROR"<<endl;
+    if ((tokenizedQuery.size() != 8 && tokenizedQuery.size() != 10) || tokenizedQuery[4] != "BY" || tokenizedQuery[6] != "IN")
+    {
+        cout << "SYNTAC ERROR" << endl;
         return false;
     }
-    parsedQuery.queryType = SORT;
+
+    if (tokenizedQuery.size() == 10) {
+        regex numeric("[+]?[0-9]+");
+        string bufferSize = tokenizedQuery[9];
+        if (tokenizedQuery[8] != "BUFFER" || !regex_match(bufferSize, numeric))
+        {
+            cout << "SYNTAX ERROR" << endl;
+            return false;
+        }
+        else
+        {
+            parsedQuery.sortBufferSize = stoi(bufferSize);
+        }
+    }
+
+    parsedQuery.queryType = JOIN;
     parsedQuery.sortResultRelationName = tokenizedQuery[0];
     parsedQuery.sortRelationName = tokenizedQuery[3];
     parsedQuery.sortColumnName = tokenizedQuery[5];
+
     string sortingStrategy = tokenizedQuery[7];
-    if(sortingStrategy == "ASC")
+    if (sortingStrategy == "ASC")
         parsedQuery.sortingStrategy = ASC;
-    else if(sortingStrategy == "DESC")
+    else if (sortingStrategy == "DESC" || sortingStrategy == "DSC")
         parsedQuery.sortingStrategy = DESC;
-    else{
-        cout<<"SYNTAX ERROR"<<endl;
+    else
+    {
+        cout << "SYNTAX ERROR" << endl;
         return false;
     }
     return true;
 }
 
-bool semanticParseSORT(){
+bool semanticParseSORT()
+{
     logger.log("semanticParseSORT");
 
-    if(tableCatalogue.isTable(parsedQuery.sortResultRelationName)){
-        cout<<"SEMANTIC ERROR: Resultant relation already exists"<<endl;
+    if (tableCatalogue.isTable(parsedQuery.sortResultRelationName))
+    {
+        cout << "SEMANTIC ERROR: Resultant relation already exists" << endl;
         return false;
     }
 
-    if(!tableCatalogue.isTable(parsedQuery.sortRelationName)){
-        cout<<"SEMANTIC ERROR: Relation doesn't exist"<<endl;
+    if (!tableCatalogue.isTable(parsedQuery.sortRelationName))
+    {
+        cout << "SEMANTIC ERROR: Relation doesn't exist" << endl;
         return false;
     }
 
-    if(!tableCatalogue.isColumnFromTable(parsedQuery.sortColumnName, parsedQuery.sortRelationName)){
-        cout<<"SEMANTIC ERROR: Column doesn't exist in relation"<<endl;
+    if (!tableCatalogue.isColumnFromTable(parsedQuery.sortColumnName, parsedQuery.sortRelationName))
+    {
+        cout << "SEMANTIC ERROR: Column doesn't exist in relation" << endl;
         return false;
     }
 
+    if (parsedQuery.sortBufferSize < 3)
+    {
+        cout << "SEMANTIC ERROR: Buffer size can't be less than 3" << endl;
+        return false;
+    }
     return true;
 }
 
-void executeSORT(){
+void executeSORT()
+{
     logger.log("executeSORT");
     return;
 }
